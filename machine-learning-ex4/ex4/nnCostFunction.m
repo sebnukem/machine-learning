d68869164b0e9031b952951dd295ffe0863665f0
Sebastien Nicoud <sebnukem@gmail.com>
2015-04-29 21:32:52 -0600

    wip

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -30,25 +30,19 @@ J = 0;
 Theta1_grad = zeros(size(Theta1));
 Theta2_grad = zeros(size(Theta2));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: You should complete the code by working through the
-%               following parts.
-%
-% Part 1: Feedforward the neural network and return the cost in the
-%         variable J. After implementing Part 1, you can verify that your
-%         cost function computation is correct by verifying the cost
-%         computed in ex4.m
-
-size(X) % 5000x400
-size(Theta1) % 25%401
-size(Theta2) % 10%26
-size(y) % 5000x1
+%size(X) % 5000x400
+%size(Theta1) % 25x401
+%size(Theta2) % 10x26
+%size(y) % 5000x1
 
 X = [ones(m, 1) X];
-Y = zeros(m, 10);
+Y = zeros(m, num_labels);
 for i = 1:m
 	Y(i,y(i)) = 1;
 end;
+
+delta1 = zeros(size(Theta1));
+delta2 = zeros(size(Theta2));
 
 for i = 1:m
 	%fw
@@ -68,8 +62,19 @@ for i = 1:m
 	%bp
 	d3 = H2 - yy;
 	d2 = d3 * Theta2 .* (a2 .* (1 - a2));
+%size(delta1) % 25x401
+%size(d2) % 1x26
+%size(a1) % 1x401
+%size(d3) % 1x10
+%size(a2) % 1x26
+	delta1 = delta1 + d2(2:end)' * a1;
+	delta2 = delta2 + d3' * a2;
+	
 end;
 J = J/m;
+
+Theta1_grad = delta1 / m;
+Theta2_grad = delta2 / m;
 
 tt1 = Theta1;
 tt1(:,1) = 0;
@@ -81,6 +86,17 @@ l = sum(tt1(:)) + sum(tt2(:));
 J = J + l * lambda / 2 / m;
 
 
+grad = [Theta1_grad(:); Theta2_grad(:)];
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: You should complete the code by working through the
+%               following parts.
+%
+% Part 1: Feedforward the neural network and return the cost in the
+%         variable J. After implementing Part 1, you can verify that your
+%         cost function computation is correct by verifying the cost
+%         computed in ex4.m
+%
 % Part 2: Implement the backpropagation algorithm to compute the gradients
 %         Theta1_grad and Theta2_grad. You should return the partial derivatives of
 %         the cost function with respect to Theta1 and Theta2 in Theta1_grad and
